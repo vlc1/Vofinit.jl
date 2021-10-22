4494054716ec7b7a47a1d3baec14d4b9af7eb885
Vincent Le Chenadec <vincentlechenadec@gmail.com>
2021-10-22 22:05:31 +0200

    according for @giordano's comment on <https://docs.julialang.org/en/v1/manual/modules/#Module-initialization-and-precompilation>

--- src/Vofi.jl
+++ src/Vofi.jl
@@ -4,6 +4,8 @@ using libvofi_jll
 
 export getcelltype, getcc
 
+const cintegrand = Ref{Ptr{Cvoid}}(0)
+
 function integrand(xyz, thunk)
     func = unsafe_pointer_to_objref(thunk)
     x = unsafe_load(xyz, 1)
@@ -13,8 +15,7 @@ function integrand(xyz, thunk)
 end
 
 function getcelltype(func, x0, h0, ndim0 = Cint(3))
-    cintegrand = @cfunction(integrand, Cdouble, (Ptr{Cdouble}, Ptr{Cvoid}))
-    @ccall libvofi.vofi_get_cell_type(cintegrand::Ptr{Cvoid},
+    @ccall libvofi.vofi_get_cell_type(cintegrand[]::Ptr{Cvoid},
                                       func::Any,
                                       x0::Ref{Cdouble},
                                       h0::Ref{Cdouble},
@@ -25,8 +26,7 @@ function getcc(func, x0, h0, xex, ndim0 = Cint(3);
                nex = Cint.((0, 0)),
                npt = Cint.((4, 4, 4, 4)),
                nvis = Cint.((0, 0)))
-    cintegrand = @cfunction(integrand, Cdouble, (Ptr{Cdouble}, Ptr{Cvoid}))
-    @ccall libvofi.vofi_get_cc(cintegrand::Ptr{Cvoid},
+    @ccall libvofi.vofi_get_cc(cintegrand[]::Ptr{Cvoid},
                                func::Any,
                                x0::Ref{Cdouble},
                                h0::Ref{Cdouble},
@@ -36,5 +36,10 @@ function getcc(func, x0, h0, xex, ndim0 = Cint(3);
                                nvis::Ref{Cint},
                                ndim0::Cint)::Cdouble
 end
+
+function __init__()
+    cintegrand[] = @cfunction(integrand, Cdouble, (Ptr{Cdouble}, Ptr{Cvoid}))
+end
+
 
 end
